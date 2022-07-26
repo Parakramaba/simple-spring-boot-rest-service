2a1b5390d35ecd614d52ea9fc6b1d472e1d1e344
Parakramaba <p.vitharana.pv@gmail.com>
2022-07-26 13:58:07 +0530

    [Test Branching]: Add new method to ExamRepository

--- src/main/java/com/parakramaba/simplespringboot/repository/ExamRepository.java
+++ src/main/java/com/parakramaba/simplespringboot/repository/ExamRepository.java
@@ -3,6 +3,10 @@ package com.parakramaba.simplespringboot.repository;
 import com.parakramaba.simplespringboot.model.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ExamRepository extends JpaRepository<Exam, Long> {
 
+    List<Exam> findAllByDate(Date date);
 }
